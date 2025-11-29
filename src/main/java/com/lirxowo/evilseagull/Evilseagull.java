2dfb4dfe7fcb94621c660db13864065d82ff2476
xiaoliziawa <3682822069@qq.com>
2025-11-29 14:02:39 +0800

    commit

---
+++ src/main/java/com/lirxowo/evilseagull/Evilseagull.java
@@ -0,0 +1,10 @@
+package com.lirxowo.evilseagull;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Evilseagull.MODID)
+public class Evilseagull {
+
+    public static final String MODID = "evilseagull";
+
+}
